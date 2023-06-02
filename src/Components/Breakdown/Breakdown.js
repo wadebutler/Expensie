9c8e7cdbac7fe28cac957a469aa47f4ac60b54e8
Wade Butler <wadebutler@Wades-MacBook-Air.local>
2023-06-02 01:04:03 -0400

    error handling

--- src/Components/Breakdown/Breakdown.js
+++ src/Components/Breakdown/Breakdown.js
@@ -5,15 +5,15 @@ const BreakDown = ({ info }) => {
   return (
     <div className="breakdown-container">
       <h2>BreakDown </h2>
-      {Object.entries(info?.expenses).map((item) => {
+      {Object.entries(info?.expenses).map((item, index) => {
         if (item[1].length > 0) {
           return (
-            <div className="breakdown-list-container">
+            <div key={index} className="breakdown-list-container">
               <h3>{item[0]}</h3>
 
               <ul>
-                {item[1].map((value) => {
-                  return <li>${value}</li>;
+                {item[1].map((value, index) => {
+                  return <li key={index}>${value}</li>;
                 })}
               </ul>
             </div>
